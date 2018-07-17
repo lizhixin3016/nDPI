6d38b99e4b47d4773a28ae8c912ec01fef5a72ea
lizhixin <lizhixin3016@aliyun.com>
2018-07-17 10:30:51 +0800

    Modify category of mongodb from cloud to database

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -1874,7 +1874,7 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
 			    ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
     ndpi_set_proto_defaults(ndpi_mod, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_MONGODB,
           no_master,
-          no_master, "MongoDB", NDPI_PROTOCOL_CATEGORY_CLOUD,
+          no_master, "MongoDB", NDPI_PROTOCOL_CATEGORY_DATABASE,
           ndpi_build_default_ports(ports_a, 27017, 0, 0, 0, 0) /* TCP */,
           ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
 
